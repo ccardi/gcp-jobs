53192ea9231ca823e73f6d771973c09c45eddc83
clement cardi <clement.cardi@gmail.com>
2023-07-05 19:21:18 +0000

    update

--- gcs-runjob/cmd.sh
+++ gcs-runjob/cmd.sh
@@ -5,5 +5,8 @@ gsutil create bucket ...
 gsutil notification create -p job-test/ -t projects/pod-fr-retail/topics/pod-fr-retail-gcs-runjob -f json -e OBJECT_FINALIZE gs://pod-fr-retail-gcs-runjob
 gsutil notification list gs://pod-fr-retail-gcs-runjob
 gsutil notification delete projects/_/buckets/pod-fr-retail-gcs-runjob/notificationConfigs/3
-
 gsutil cp config.json gs://pod-fr-retail-gcs-runjob
+
+
+gcloud workflows deploy createrunjob \
+    --source=workflows-createrunjob.yaml \
